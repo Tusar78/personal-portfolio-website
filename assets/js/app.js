6e47fedb65ab2d4f496163feba767ddb65fcc02e
Tusar <tusarahmed141@gmail.com>
2022-07-06 17:40:28 +0600

    Added toggle feature in navigation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,23 @@
 /*==== Menu show vertically hidden ====*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
 
 /*==== MENU SHOW ====*/
 /* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', event => {
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*==== MENU HIDDEN ====*/
 /* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', event => {
+    navMenu.classList.remove('show-menu');
+  })
+}
 
 /*==== Remove menu in mobile screen ====*/
 
