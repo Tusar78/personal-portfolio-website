db23aed044be0f29f8b1f6d53be5924533189dd0
Tusar <tusarahmed141@gmail.com>
2022-07-06 18:08:07 +0600

    When we click on each navlink, then we remove the show menu class

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,35 @@
 /*==== Menu show vertically hidden ====*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*==== MENU SHOW ====*/
 /* Validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener('click', event => {
-    navMenu.classList.add('show-menu')
-  })
+  navToggle.addEventListener("click", (event) => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*==== MENU HIDDEN ====*/
 /* Validate if constant exists */
 if (navClose) {
-  navClose.addEventListener('click', event => {
-    navMenu.classList.remove('show-menu');
-  })
+  navClose.addEventListener("click", (event) => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==== Remove menu in mobile screen ====*/
+const navLinks = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+
+  // When we click on each "nav__link", then we remove the "show-menu" class.
+  navMenu.classList.remove("show-menu");
+};
+
+navLinks.forEach((link) => link.addEventListener("click", linkAction));
 
 /*==== Accordion design upon skills ====*/
 
