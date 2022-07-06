62e8529a30c45c401d7023b118604e097c3fefff
Tusar <tusarahmed141@gmail.com>
2022-07-06 16:03:14 +0600

    Connect our js file

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+/*==== Menu show vertically hidden ====*/
+
+/*==== MENU SHOW ====*/
+/* Validate if constant exists */
+
+/*==== MENU HIDDEN ====*/
+/* Validate if constant exists */
+
+/*==== Remove menu in mobile screen ====*/
+
+/*==== Accordion design upon skills ====*/
+
+/*==== Qualifications Tab ====*/
+
+/*==== Service Modal ====*/
+
+/*==== Portfolio swiper ====*/
+
+/*==== Testimonial ====*/
+
+/*==== Scroll section active link ====*/
+
+/*==== Change background header ====*/
+
+/*==== Shoe scroll up button ====*/
+
+/*==== Dark/Night Theme ====*/
