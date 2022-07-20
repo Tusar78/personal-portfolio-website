b61cd2d85beafec59c8ae1a8bd2f74730cd56453
Tusar <tusarahmed141@gmail.com>
2022-07-20 19:46:59 +0600

    Qualification section is done

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,8 +53,26 @@ skillsHeader.forEach((element) => {
 });
 
 /*==== Qualifications Tab ====*/
+const tabs = document.querySelectorAll('[data-target]')
+      tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('qualification__active');
+    })
 
+    target.classList.add('qualification__active');
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active')
+    })
+
+    tab.classList.add('qualification__active')
+  })
+})
 /*==== Service Modal ====*/
 
 /*==== Portfolio swiper ====*/
