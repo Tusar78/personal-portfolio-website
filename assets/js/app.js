0c283550145b1a45a7d19c6f29bca6bb3ff93784
Tusar <tusarahmed141@gmail.com>
2022-07-21 22:16:10 +0600

    Services area is done

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,7 +74,27 @@ tabs.forEach(tab => {
   })
 })
 /*==== Service Modal ====*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalButtons = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.service__modal-close');
 
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal');
+}
+
+modalButtons.forEach((modalButton, i) => {
+  modalButton.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalCloses.forEach(modalClose => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach(modalView => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
 /*==== Portfolio swiper ====*/
 
 /*==== Testimonial ====*/
